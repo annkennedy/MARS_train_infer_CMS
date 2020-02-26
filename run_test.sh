40bdfd5c6413cb8354824756faa74033398ddedc
Matt Levine <mattlevine22@gmail.com>
2020-02-26 15:18:10 -0800

    new script for running tests on the HPC

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #
-#SBATCH --time=60:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=192G   # memory per CPU core
+#SBATCH -c 1 # 1 core per task
+#SBATCH --mem-per-cpu=2G   # memory per CPU core
 #SBATCH -J "run_training"   # job name
 #SBATCH --mail-user=mlevine@caltech.edu   # email address
 
 source activate mars_tf
 cd ~/MARS_train_infer_CMS/
-python run_training.py sniff_face both > test_output/mars_log.txt
+
+# Execute jobs in parallel
+srun --exclusive -N 1 -n 1 python test_run.py --lr 0.1
+wait
