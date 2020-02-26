b473436ccc4223a6ca45ccbc78a13b6325655a64
Matt Levine <mattlevine22@gmail.com>
2020-02-26 15:20:23 -0800

    new run test script

--- run_test.sh
+++ run_test.sh
@@ -12,5 +12,5 @@ source activate mars_tf
 cd ~/MARS_train_infer_CMS/
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python test_run.py --lr 0.1
+srun --exclusive -N 1 -n 1 python test_run.py --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/
 wait
