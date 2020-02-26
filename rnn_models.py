419a38042dccffc0426c6cc94bff45b46f1c1442
Matt Levine <mattlevine22@gmail.com>
2020-02-26 15:17:13 -0800

    adding module for RNN stuff

---
+++ rnn_models.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class LSTMTagger(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, num_classes):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(input_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, input_sequence):
+        lstm_out, _ = self.lstm(input_sequence.view(input_sequence.shape[0], 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(input_sequence.shape[0], -1))
+        predicted_class_scores = F.log_softmax(tag_space, dim=1)
+        return predicted_class_scores
+
+def get_optimizer(name, params, lr):
+    if name=='SGD':
+        return optim.SGD(params, lr=lr)
+    else:
+        return None
+
+def get_loss(name, params=None):
+    if name=='nn.NLLLoss':
+        return nn.NLLLoss()
+    else:
+        return None
+
+def get_model(name, input_dim, hidden_dim, num_classes):
+    if name=='LSTMTagger':
+        return LSTMTagger(input_dim=input_dim, hidden_dim=hidden_dim, num_classes=num_classes)
+    else:
+        return None
+
