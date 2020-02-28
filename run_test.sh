546822562f3b806275313447faeb54a64d4726cd
Matt Levine <mattlevine22@gmail.com>
2020-02-28 11:52:36 -0800

    more gpu settings

--- run_test.sh
+++ run_test.sh
@@ -2,11 +2,10 @@
 #
 #SBATCH --time=48:00:00   # walltime
 #SBATCH --gres=gpu:3
+#SBATCH --gpus-per-task=1
+#SBATCH --ntasks=3
 #SBATCH -J "run_GPU_training"   # job name
 #SBATCH --mail-user=mlevine@caltech.edu   # email address
-
-source activate mars_tf
-cd ~/MARS_train_infer_CMS/
 
 # Execute jobs in parallel
 srun --exclusive -N 1 -n 1 python test_run.py --use_gpu True --lr 0.1 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/ --output_path ./gpu_test_output_lr0.1 &
