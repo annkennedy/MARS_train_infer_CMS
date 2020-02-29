97a337b97dc6fe54290862327ae2809988c8c0d8
Matt Levine <mattlevine22@gmail.com>
2020-02-28 18:04:06 -0800

    only run 100 epochs

--- sweep_optimizers.sh
+++ sweep_optimizers.sh
@@ -5,8 +5,8 @@
 #SBATCH --gid=andersonlab
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_SGD --optimizer SGD --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
-srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_RMSprop --optimizer RMSprop --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
-srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_Adam --optimizer Adam --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
-srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_LBFGS --optimizer LBFGS --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/
+srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_SGD --optimizer SGD --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_RMSprop --optimizer RMSprop --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_Adam --optimizer Adam --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 --gpus 1 python test_run.py --output_path ./test_output_LBFGS --optimizer LBFGS --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/
 wait
