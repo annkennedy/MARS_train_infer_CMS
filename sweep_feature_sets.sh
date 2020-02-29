99574da5c728d37d7215f4c99e6bd904998d95e3
Matt Levine <mattlevine22@gmail.com>
2020-02-28 20:31:51 -0800

    longer sweep runs

--- sweep_feature_sets.sh
+++ sweep_feature_sets.sh
@@ -5,7 +5,7 @@
 #SBATCH --gid=andersonlab
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_featurestyle_all --feature_style 'all' --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
-srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_featurestyle_keypoints_only --feature_style 'keypoints_only' --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
-srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_featurestyle_selective --feature_style 'selective' --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_featurestyle_1000epochs_all --feature_style 'all' --lr 0.1 --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_featurestyle_1000epochs_keypoints_only --feature_style 'keypoints_only' --lr 0.1 --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_featurestyle_1000epochs_selective --feature_style 'selective' --lr 0.1 --use_gpu True --num_epochs 1000 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/
 wait
