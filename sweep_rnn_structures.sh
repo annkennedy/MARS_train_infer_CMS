4c11f92fb35681aa6551026713fa27267d4b9234
Matt Levine <mattlevine22@gmail.com>
2020-03-03 17:10:13 -0800

    test rnn structures with run script

---
+++ sweep_rnn_structures.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://s3-us-west-2.amazonaws.com/imss-hpc/index.html to generate script
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --gres=gpu:1
+#SBATCH --gid=andersonlab
+
+# Execute jobs in parallel
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_rnn_LSTM_backwards --model_name LSTMTagger --bidirectional True --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_rnn_GRU --model_name GRUTagger --bidirectional False --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_rnn_GRU_backwards --model_name GRUTagger --bidirectional True --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/  &
+srun --exclusive -N 1 -n 1 python test_run.py --output_path ./test_output_rnn_LSTM --model_name LSTMTagger --bidirectional False --lr 0.1 --use_gpu True --num_epochs 100 --train_path /groups/Andersonlab/CMS273/TRAIN_lite/ --test_path /groups/Andersonlab/CMS273/TEST_lite/
+wait
